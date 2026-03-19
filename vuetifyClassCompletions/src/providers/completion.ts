5b2fe5aba21c1827bfd51ad546b5539b067cddeb
Julian Lord <julian.lord@vastech.co.za>
2026-03-19 08:13:40 +0200

    Fixed some of the classes and added color handling

--- vuetifyClassCompletions/src/providers/completion.ts
+++ vuetifyClassCompletions/src/providers/completion.ts
@@ -29,13 +29,27 @@ function createItem(
         description: "Vuetify utility class",
     };
 
+    const isColor =
+        classInfo.type === "color" &&
+        classInfo.color !== undefined &&
+        classInfo.color.trim().length > 0 &&
+        classInfo.color.trim().startsWith("#");
+
     const item = new vscode.CompletionItem(
         completionLabel,
-        vscode.CompletionItemKind.Constant,
+        isColor
+            ? vscode.CompletionItemKind.Color
+            : vscode.CompletionItemKind.Constant,
     );
 
     item.insertText = label;
-    if (classInfo.css !== undefined && classInfo.css.trim().length !== 0) {
+
+    if (isColor) {
+        item.documentation = new vscode.MarkdownString(
+            `Color: \`${classInfo.color}\``,
+        );
+        item.detail = classInfo.color;
+    } else if (classInfo.css?.trim().length) {
         item.detail = classInfo.css;
     }
 
